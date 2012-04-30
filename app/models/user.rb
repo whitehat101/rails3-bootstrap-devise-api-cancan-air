e6bf14b600213b9dfc1f00ce3eb4cf21e1f6ae5d
Jeremy Ebler <Jeremy@JaguarDesignStudio.com>
2012-04-30 01:22:07 -0700

    Include devise auth token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
-	rolify
+  before_save :ensure_authentication_token
+  rolify
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :token_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
